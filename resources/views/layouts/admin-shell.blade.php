d9cf2c4a701820299d5e93c0f60592c83319d731
rizkyfauzi505 <rizkyfauzinuryana07@gmail.com>
2025-08-25 12:49:59 +0700

    profil dashboard

--- resources/views/layouts/admin-shell.blade.php
+++ resources/views/layouts/admin-shell.blade.php
@@ -72,7 +72,7 @@
         <!-- Topbar -->
         <div class="topbar">
             <div class="profile">
-                <img src="{{ session('auth_photo') }}" alt="Foto Profil">
+                 <img src="{{ asset('img/poto.jpg') }}" alt="Foto Profil" class="profile-img">
                 <div class="info">
                     <strong>{{ session('auth_name') }}</strong>
                     <span>{{ ucfirst(session('auth_role')) }}</span>
