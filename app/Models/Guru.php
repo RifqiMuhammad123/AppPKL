8008643fcadb2d5cbf8cda53ed18704f9cf5ea39
Navidkia7 <navidkia2007@gmail.com>
2025-08-25 11:38:48 +0700

    nu bener

--- app/Models/Guru.php
+++ app/Models/Guru.php
@@ -3,15 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Guru extends Model
+class Guru extends Authenticatable
 {
     use HasFactory;
 
     protected $table = 'guru';
-    protected $primaryKey = 'id_guru';
-    protected $fillable = ['nip','nama_guru','password'];
+    protected $primaryKey = 'id_guru'; // sesuaikan dengan primary key di tabel kamu
+
+    protected $fillable = ['nip', 'nama_guru', 'password'];
+
+    public $timestamps = false; // 🚀 matikan created_at & updated_at
 
     /** Relasi ke Permintaan */
     public function permintaan()
